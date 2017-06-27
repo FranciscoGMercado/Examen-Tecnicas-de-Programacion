a61ab1cd9bf8417c8dc193afcce0c708a65cf1de
Gerado <franciscoklf@netscape.net>
2017-06-27 15:50:12 -0300

    no sale
    hasta aca llegue. cuando quiero darle un valor especifico a un elemento de la matriz el resto de la fila desaparece.

--- ej3.py
+++ ej3.py
@@ -1,9 +1,18 @@
-ruta = str(input('Ingrese una ruta de archivo valida: '))
-extencion = str(input('Ingrese una extencion de archivo valida: '))
+import random
 def ej3():
-    if(extencion in ruta):
-        print('es una ruta valida.')
-    else:
-        print('No es una ruta valida para esa extencion!!!')
-        print(ruta)
-ej3()
+    L = []
+    J = []
+    M = []
+    matriz = [[L], [J], [M]]
+    N = int(4)
+    valor = int(2)
+    for i in range(N):
+        L.append(random.randint(0,100))
+    for i in range(N):
+        J.append(random.randint(0,100))
+    for i in range(N):
+        M.append(random.randint(0,100))
+    print(matriz[0][0:])
+    print(matriz[1][0:])
+    print(matriz[2][0:])
+print(ej3())
